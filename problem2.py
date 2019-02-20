b686495a46eec0358711195c0e06fb2238440226
Michael Urban <urban22@gmail.com>
2019-02-20 12:15:48 -0500

    problem 2

--- problem2.py
+++ problem2.py
@@ -4,10 +4,29 @@
 
 # How do you find the duplicate number on a given integer array? 
 
-# 
+# solving without using packages
 
 #%%
 testArray = list(range(10))
+testArray[2] = 5 # inserting 5 as a duplicate
 print(testArray)
+
+#%%
+tempArray = sorted(testArray)
+print(tempArray)
+
+#%%
+newArray = []
+dupeArray = []
+for element in tempArray:
+    if element not in newArray:
+        newArray.append(element)
+    else:
+        dupeArray.append(element)
+
+print('The array with duplicates removed is: ')
+print(newArray)
+print('The duplicates found are: ')
+print(dupeArray)
 
 #%%
