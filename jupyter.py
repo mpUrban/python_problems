09f5f02ba3172edfbf390eadaf4bce5f2cc1f03c
Michael Urban <urban22@gmail.com>
2019-02-20 11:53:14 -0500

    keyboard config

--- jupyter.py
+++ jupyter.py
@@ -2,6 +2,10 @@
 # Jupyter:
 # Python: Select Interpreter command from the Command Palette (⇧⌘P).
 
+# To configure keyboard shortcuts the way you want, open Keyboard Shortcuts editor and click on the link keybindings.json.
+
+#  https://code.visualstudio.com/docs/getstarted/keybindings
+
 #%%
 msg = "Hello World"
 print(msg)
---
+++ problem2.py
@@ -0,0 +1,13 @@
+# written in VS Code with jupyter extension
+
+#https://simpleprogrammer.com/programming-interview-questions/
+
+# How do you find the duplicate number on a given integer array? 
+
+# 
+
+#%%
+testArray = list(range(10))
+print(testArray)
+
+#%%
